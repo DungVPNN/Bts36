d1b58f9c9829971b1e294d46c6947a9214d3aa20
DungVPNN <dungbgbg12tt@gmail.com>
2024-05-23 02:54:35 +0700

    ss36

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Bai1 from './components/ss36/Bai1'
+import Bai2 from './components/ss36/Bai2'
+import Bai3 from './components/ss36/Bai3'
+import Bai5 from './components/ss36/Bai5'
+import Bai6 from './components/ss36/Bai6'
+import Bai7 from './components/ss36/Bai7'
+import Bai8 from './components/ss36/Bai8'
+import Bai9 from './components/ss36/Bai9'
+import Bai10 from './components/ss36/Bai10'
+import UseEffect from './components/UseEffect'
+
+export default function App() {
+  return (
+    <div>
+      <UseEffect></UseEffect>
+      <Bai1></Bai1>
+      <Bai2></Bai2>
+      <Bai3></Bai3>
+      <Bai5></Bai5>
+      <Bai6></Bai6>
+      <Bai7></Bai7>
+      <Bai8></Bai8>
+      <Bai9></Bai9>
+      <Bai10></Bai10>
+    </div>
+  )
+}
